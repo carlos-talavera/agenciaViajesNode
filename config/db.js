d7418084caf39982c04e5c3e42c3936af546d410
carlos-talavera <carloseduardorivera12@gmail.com>
2023-06-09 22:55:03 -0600

    SSL habilitado

--- config/db.js
+++ config/db.js
@@ -5,6 +5,9 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: true
+    },
     define: {
         timestamps: false
     },
